691a831fb20fdd99653cea7b7b5ea3b1fdf29b9b
Ben Rowe <ben.rowe.83@gmail.com>
2016-12-27 15:43:40 +1100

    Create LastFm factory + unit tests

---
+++ tests/Services/LastFm/FactoryTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ */
+
+namespace Tests\Services\LastFm;
+
+use App\Services\LastFm\Factory;
+
+class FactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFactory()
+    {
+        $client = Factory::fromConfig(
+            [
+                'api_key'    => '12345',
+                'api_secret' => 'secret',
+                'pagination' => ['limit' => 100]
+            ]
+        );
+        $this->assertSame('12345', $client->getApiKey());
+        $this->assertArrayHasKey('pagination', $client->getOptions());
+    }
+}
