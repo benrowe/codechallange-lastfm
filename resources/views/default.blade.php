dcf6eee3358361be726f1fb9a8e612e36c11adcc
Ben Rowe <ben.rowe.83@gmail.com>
2016-12-27 14:24:34 +1100

    Fix indenting of home blade file

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -3,16 +3,16 @@
 @section('title', 'Top artists per country')
 
 @section('content')
-@if ($results)
-    <h1>Top Artists Per Country: {{ $model->country()->name }}</h1>
-    <ul>
-        @foreach ($results as $artist)
-            @include('partials.artist', ['artist' => $artist])
-        @endforeach
-    </ul>
-    @include('partials.pagination', ['resultSet' => $results])
-@else
-    Please select a country
-@endif
     @include('partials.form', ['model' => $model])
+    @if ($results)
+        <h1>Top Artists Per Country: {{ $model->country()->name }}</h1>
+        <ul>
+            @foreach ($results as $artist)
+                @include('partials.artist', ['artist' => $artist])
+            @endforeach
+        </ul>
+        @include('partials.pagination', ['resultSet' => $results])
+    @else
+        Please select a country
+    @endif
 @endsection
