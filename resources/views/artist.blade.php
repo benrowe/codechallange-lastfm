e41c1f4076b9b3babeefa837a236f1d21b697aa8
Ben Rowe <ben.rowe.83@gmail.com>
2016-12-27 14:25:03 +1100

    Add markup for artist blade

--- resources/views/artist.blade.php
+++ resources/views/artist.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.artist')
 
+@section('title', $artist->name)
+
 @section('content')
-    <img src="{{$artist->image(\App\Services\LastFm\Response\Artist::IMAGE_SIZE_EXTRA_LARGE)}}" alt=""/>
-    {{$artist->name}}
-    @include('partials.artist-tracks', ['tracks' => $artist->topTracks()])
+    <div class="row">
+        <div class="col-sm-4">
+            <img src="{{$artist->image(\App\Services\LastFm\Response\Artist::IMAGE_SIZE_EXTRA_LARGE)}}" alt=""/>
+        </div>
+        <div class="col-sm-8">
+            <h1>{{ $artist->name }}</h1>
+            @include('partials.artist-tracks', ['tracks' => $artist->topTracks(), 'artist' => $artist])
+        </div>
+    </div>
+
 @endsection
