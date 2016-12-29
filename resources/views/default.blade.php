433f935934a322c5e37aa1dc2ad75c707946d886
Ben Rowe <ben.rowe.83@gmail.com>
2016-12-29 14:06:48 +1100

    Update logic statements in blade files

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -4,16 +4,20 @@
 
 @section('content')
     @include('partials.form', ['model' => $model])
-    @if ($results)
+    @if ($results !== null)
         <h1>Top Artists Per Country: {{ $model->country()->name }}</h1>
-        <ol>
-            @foreach ($results as $artist)
-                <li>
-                    @include('partials.artist', ['artist' => $artist])
-                </li>
-            @endforeach
-        </ol>
-        @include('partials.pagination', ['resultSet' => $results, 'url' => '?SearchForm%5Bcountry%5D='.$model->country.'&'])
+        @if (count($results) > 0)
+            <ul>
+                @foreach ($results as $pos => $artist)
+                    <li>
+                        @include('partials.artist', ['artist' => $artist])
+                    </li>
+                @endforeach
+            </ul>
+            @include('partials.pagination', ['resultSet' => $results, 'url' => '?SearchForm%5Bcountry%5D='.$model->country.'&'])
+        @else
+            <p>No results can be found for {{ $model->country()->name }}</p>
+        @endif
     @else
         Please select a country
     @endif
--- resources/views/partials/pagination.blade.php
+++ resources/views/partials/pagination.blade.php
@@ -1,8 +1,10 @@
-@if ($resultSet->currentPage() > 1)
-    <a href="{{$url}}page={{ $resultSet->currentPage() - 1 }}">Prev</a>
-@endif
-Page {{ $resultSet->currentPage() }} of {{ $resultSet->totalPages() }}
+@if ($resultSet->totalPages() > 0)
+    @if ($resultSet->currentPage() > 1)
+        <a href="{{$url}}page={{ $resultSet->currentPage() - 1 }}">Prev</a>
+    @endif
+    Page {{ $resultSet->currentPage() }} of {{ $resultSet->totalPages() }}
 
-@if ($resultSet->currentPage() < $resultSet->totalPages())
-    <a href="{{$url}}page={{  $resultSet->currentPage() + 1 }}">Next</a>
+    @if ($resultSet->currentPage() < $resultSet->totalPages())
+        <a href="{{$url}}page={{  $resultSet->currentPage() + 1 }}">Next</a>
+    @endif
 @endif
